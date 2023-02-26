328c063d233cd4a3e54cf4ee4b36808bad0dc361
estebansolanog <estebansolanoguillen@gmail.com>
2023-02-26 20:13:06 +0000

    Tarea Finalizada

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("button").addEventListener("click", () => {
+    document.querySelector(".card").classList.add(generateRandomSuit());
+    document.querySelector(".number").innerHTML = generateRandomNumber();
+  });
+};
+
+const generateRandomNumber = () => {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "9",
+    "10",
+    "Q",
+    "K"
+  ];
+  let cardNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[cardNumber];
+};
+
+const generateRandomSuit = () => {
+  const suit = ["spade", "diam", "heart", "club"];
+  let cardSuit = Math.floor(Math.random() * suit.length) + 1;
+  return suit[cardSuit];
 };
